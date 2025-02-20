01004aac6f5de97dcd54d726a0693e37c780ec4c
mihael-tunik <mihael.8112@yahoo.com>
2025-02-21 02:47:54 +0300

    Make the code beautiful and update description

--- README.md
+++ README.md
@@ -1,5 +1,31 @@
+DP on masks or Subset DP is a technique to decrease O(n!) complexity of algorithms.
+Here's collection of nice examples. 
+
 Problem list:
 
 - max_sum 
 Given n x n chess board with numbers. 
 Place n rooks so sum on positions with maximal sum.
+
+Solution:
+
+```
+Store two arrays: dp[2^n - 1], chosen[2^n - 1]
+
+
+for k in 1..n
+    for mask in combinations(k)
+        choose index i not in mask (mask & (1<<i) is zero)
+            choose minimum a[i][j] among columns not chosen (chosen[mask])
+               update dp, chosen when necessary
+```
+
+Complexity: O(n^2 2^n) time, O(2^n) space.
+
+Tested:
+```
+n  T (dp) T (n!)
+12 0.6ms  3s
+20 0.3s   >>
+30 500s   >>
+```
--- max_sum.cpp
+++ max_sum.cpp
@@ -63,52 +63,28 @@ inline int next_comb(int v) {
     return (t + 1) | (((~t & -~t) - 1) >> (__builtin_ctz(v) + 1));
 }
 
-// O(n^3 2^n) time and O(2^n) space
+// O(n^2 2^n) time and O(2^n) space
 int min_sum_2d_dp(vector <vector <int>> &a){
     int n = a.size(), INF = 1e9;
-
     vector <int> dp(1 << n, 0);
     vector <int> choice(1 << n, 0);
-    vector <int> init(1 << n, 0);
-    
-    init[0] = 1;
 
     for (int k = 0; k <= n; ++k) {
         for (int mask = (1<<k)-1; mask < (1<<n); mask = next_comb(mask)) {
-
-            //for(int i = 0; i < n; ++i)
-            //    printf("%i ", (mask >> i)%2);
-            //printf("\n");
-            
-
             for(int i = 0; i < n; ++i){
-
                 if ((mask & (1 << i)) == 0){
-                    int min_in_col = INF, l = -1;
+                    int min_in_col = INF, l = -1, mask_i = mask + (1 << i);
                     
-                    for (int j = 0; j < n; ++j){
-                        if ((choice[mask] & (1 << j)) == 0 && (a[i][j] < min_in_col)){
-                            l = j;
-                            min_in_col = a[i][j];
-                        }
-                    }
+                    for (int j = 0; j < n; ++j)
+                        if ((choice[mask] & (1 << j)) == 0 && (a[i][j] < min_in_col))
+                            l = j, min_in_col = a[i][j];
                             
-                    int mask_i = mask + (1 << i); 
-
-                    if (init[mask_i] == 0){
+                    if (choice[mask_i] == 0 || dp[mask] + min_in_col < dp[mask_i] ){
                         dp[mask_i] = dp[mask] + min_in_col;
                         choice[mask_i] = choice[mask] + (1 << l);
-                        init[mask_i] = 1;
-                    }
-                    else{                    
-                        if (dp[mask] + min_in_col < dp[mask_i]){
-                            dp[mask_i] = dp[mask] + min_in_col;
-                            choice[mask_i] = choice[mask] + (1 << l);
-                        }
-                    }        
+                    }    
                 }
-            }
-            
+            }            
             if(mask == 0)
                break;
         }
@@ -137,10 +113,10 @@ int min_sum_2d_dp(vector <vector <int>> &a){
 }
 
 int main(void){
-    int n = 30;
+    int n = 12;
     vector <vector <int>> a(n);
     
-    //srand(time(NULL));
+    srand(time(NULL));
 
     for(int i = 0; i < n; ++i){
         a[i].resize(n);
@@ -149,7 +125,7 @@ int main(void){
     }
    
     auto s1 = chrono::high_resolution_clock::now();
-    //min_sum_2d_naive(a);
+    min_sum_2d_naive(a);
     auto s2 = chrono::high_resolution_clock::now();
     
     printf("Ready in %lf s.\n", chrono::duration<double, milli>(s2-s1).count()/1000);     
@@ -162,4 +138,3 @@ int main(void){
 
     return 0;
 }
-
